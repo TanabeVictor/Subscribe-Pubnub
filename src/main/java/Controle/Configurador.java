4392014147e305c1268b7d3ed1e670168a557a71
Tanabe <tanabebr@gmail.com>
2018-09-02 02:47:45 -0300

    Retirada de Classes Desnecessárias e Filtro Implementado

--- src/main/java/Controle/Configurador.java
+++ src/main/java/Controle/Configurador.java
@@ -12,6 +12,7 @@ import java.util.Arrays;
 import java.util.UUID;
 
 public class Configurador {
+
     final String PubKey = "pub-c-3701cc0b-3e9b-4f08-aad4-fb2c0a7de660";
     final String SubKey = "sub-c-bdb4e6fa-a701-11e8-b082-12b6fee23487";
     String UserId = null;
@@ -19,60 +20,56 @@ public class Configurador {
     String aux = uuid.toString();
     final String kChannel = "Teste";
     PubNub pubnub;
-    
-    class complexData {
-    String fieldA;
-    int fieldB;}
-    
-    AddMensagem ctrMensagem;
-    public Configurador(){
-        PNConfiguration pnConfiguration = new PNConfiguration();
-        pnConfiguration.setPublishKey(PubKey);
-        pnConfiguration.setSubscribeKey(SubKey);
+
+    public Configurador() {
+        PNConfiguration config = new PNConfiguration();
+        config.setFilterExpression("Temperatura > 50");
+        config.setSubscribeKey(SubKey);
         UserId = "PubNubID" + createUserID(aux);
-        pnConfiguration.setUuid(UserId);
-        pubnub= new PubNub(pnConfiguration);}
+        config.setUuid(UserId);
+        pubnub = new PubNub(config);
+    }
 
     public void subscribe() {
-    try {
-    pubnub.addListener(new SubscribeCallback() {
-    @Override
-    public void status(PubNub pubnub, PNStatus status) {
-        if (status.getCategory() == PNStatusCategory.PNConnectedCategory){
-            complexData data = new complexData();
-            data.fieldA = "Awesome";
-            data.fieldB = 10;
-            pubnub.setPresenceState()
-                .channels(Arrays.asList(kChannel)) // subscribe to channels
-                .state(data).async(new PNCallback<PNSetStateResult>() {
-                   
-                    @Override
-                    public void onResponse(PNSetStateResult result, PNStatus status) {
-  
+        try {
+            pubnub.addListener(new SubscribeCallback() {
+                @Override
+                public void status(PubNub pubnub, PNStatus status) {
+                    if (status.getCategory() == PNStatusCategory.PNConnectedCategory) {
+                        pubnub.setPresenceState()
+                                .channels(Arrays.asList(kChannel)) // subscribe to channels
+                                .async(new PNCallback<PNSetStateResult>() {
+
+                                    @Override
+                                    public void onResponse(PNSetStateResult result, PNStatus status) {
+
+                                    }
+                                });
                     }
-                });
+                }
+
+                @Override
+                public void message(PubNub pubnub, PNMessageResult message) {
+                    System.out.println(message);
+                }
+
+                @Override
+                public void presence(PubNub pubnub, PNPresenceEventResult presence) {
+                }
+            });
+            pubnub.subscribe()
+                    .channels(Arrays.asList(kChannel))
+                    .execute();
+        } catch (Exception e) {
+            System.out.println("Erro no Subscribe");
         }
     }
- 
-    @Override
-    public void message(PubNub pubnub, PNMessageResult message) {
-        System.out.println(message);}
- 
-    @Override
-    public void presence(PubNub pubnub, PNPresenceEventResult presence) {
- 
-    }});
-                      pubnub.subscribe()
-                      .channels(Arrays.asList(kChannel))
-                      .execute();
-              } catch (Exception e) {
-			System.out.println("Erro no Subscribe");
-		}
-	}
-        
-    public int createUserID(String aux){
+
+    public int createUserID(String aux) {
         int hash = 7;
         for (int i = 0; i < aux.length(); i++) {
-        hash = hash*31 + aux.charAt(i);}
-        return hash;}
+            hash = hash * 31 + aux.charAt(i);
+        }
+        return hash;
+    }
 }
